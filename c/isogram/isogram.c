d2edf53aa31517460eddd6e1fc6a4872dca32e81
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2022-08-21 02:31:33 -0300

    :construction: chore: first test passing

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -1,1 +1,20 @@
 #include "isogram.h"
+
+bool is_isogram(const char phrase[])
+{
+    if (phrase == NULL)
+    {
+        return false;
+    }
+
+    char c;
+    int i = 0;
+    
+    do
+    {
+        c = phrase[i];
+        i++;
+    } while (c != '\0');
+    
+    return true;
+}
