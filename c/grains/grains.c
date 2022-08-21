743789488a92e1c503617ed7bb77ba7e0c3b8cac
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2022-08-21 03:48:05 -0300

    :heavy_check_mark: all tests passing

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -3,5 +3,18 @@
 
 uint64_t square(uint8_t index)
 {
+    if (index <= 0) return 0;
+    if (index >= 65) return 0;
     return pow(2, index - 1);
+}
+
+uint64_t total(void)
+{
+    uint64_t sum = 0;
+
+    for (int i = 0; i < 65; i++)
+    {
+        sum += square(i);
+    }
+    return sum;
 }
