526a48e67a485da9fd5bb4e3dbaf0c2939e25c08
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2022-08-21 00:00:31 -0300

    :checkered_flag: start ecercise

---
+++ c/resistor-color/resistor_color.h
@@ -0,0 +1,17 @@
+#ifndef RESISTOR_COLOR_H
+#define RESISTOR_COLOR_H
+
+typedef enum {
+    BLACK,
+    BROWN,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    VIOLET,
+    GREY,
+    WHITE
+} resistor_band_t;
+
+#endif
