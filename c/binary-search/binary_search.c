e783da048753ba2ae1f75b20fbb25a80bbb9c9be
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2022-09-05 00:56:35 -0300

    :construction: 1st test passing

--- c/binary-search/binary_search.c
+++ c/binary-search/binary_search.c
@@ -1,1 +1,13 @@
 #include "binary_search.h"
+
+int *binary_search(int value, const int *arr, size_t length)
+{
+    int start = length / 2;
+    
+    if (value == arr[start])
+    {
+        return (int*)(arr + start);
+    }
+    
+    return NULL;
+}
