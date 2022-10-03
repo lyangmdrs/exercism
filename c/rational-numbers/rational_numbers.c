cdf900ea591a8f3cefc4d0518ca3e341f47864a2
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2022-10-03 02:04:56 -0300

    :construction: 1st test passing

--- c/rational-numbers/rational_numbers.c
+++ c/rational-numbers/rational_numbers.c
@@ -1,1 +1,9 @@
 #include "rational_numbers.h"
+
+rational_t add(rational_t r1, rational_t r2)
+{
+    rational_t sum;
+    sum.numerator = r1.numerator * r2.denominator + r2.numerator * r1.denominator;
+    sum.denominator = r1.denominator * r2.denominator;
+    return sum;
+}
--- c/rational-numbers/rational_numbers.h
+++ c/rational-numbers/rational_numbers.h
@@ -1,4 +1,21 @@
 #ifndef RATIONAL_NUMBERS_H
 #define RATIONAL_NUMBERS_H
 
+#include <stdint.h>
+
+typedef struct 
+{
+    int numerator;
+    int denominator;
+}rational_t;
+
+rational_t add(rational_t r1, rational_t r2);
+rational_t subtract(rational_t r1, rational_t r2);
+rational_t multiply(rational_t r1, rational_t r2);
+rational_t divide(rational_t r1, rational_t r2);
+rational_t absolute(rational_t r);
+float exp_real(uint16_t r1, rational_t r2);
+rational_t exp_rational(rational_t r, int16_t n);
+rational_t reduce(rational_t r);
+
 #endif
