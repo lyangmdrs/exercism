27d4b00c59c98c46636e4955690018effba29076
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2022-11-16 23:35:32 -0300

    :construction: dummy declaration of functions

--- c/circular-buffer/circular_buffer.c
+++ c/circular-buffer/circular_buffer.c
@@ -1,1 +1,22 @@
 #include "circular_buffer.h"
+
+int16_t overwrite(circular_buffer_t* buffer, buffer_value_t value)
+{
+    circular_buffer_t a;
+    int16_t status = value;
+    a.a = value;
+    buffer->a = a.a;
+    return status;
+}
+
+circular_buffer_t* new_circular_buffer(size_t capacity)
+{
+    static circular_buffer_t a;
+    a.a = capacity;
+    return &a;
+}
+
+void delete_buffer(circular_buffer_t* buffer)
+{
+    buffer->a = 0;
+}
