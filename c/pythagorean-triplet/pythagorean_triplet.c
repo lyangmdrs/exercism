76c746927680b134cc5baa9776247dfda8db2ffd
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-01-09 23:34:32 -0300

    :construction: 1st test passing

--- c/pythagorean-triplet/pythagorean_triplet.c
+++ c/pythagorean-triplet/pythagorean_triplet.c
@@ -1,1 +1,16 @@
 #include "pythagorean_triplet.h"
+
+triplets_t* triplets_with_sum(uint16_t n)
+{
+    triplets_t* triplet = malloc(sizeof(triplets_t) + sizeof(triplet_t));
+    triplet->count = n/12;
+    triplet->triplets[0].a = 3;
+    triplet->triplets[0].b = 4;
+    triplet->triplets[0].c = 5;
+
+    return triplet;
+}
+void free_triplets(triplets_t *triplets)
+{
+    free(triplets);
+}
--- c/pythagorean-triplet/pythagorean_triplet.h
+++ c/pythagorean-triplet/pythagorean_triplet.h
@@ -1,4 +1,21 @@
 #ifndef PYTHAGOREAN_TRIPLET_H
 #define PYTHAGOREAN_TRIPLET_H
 
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct {
+   uint16_t a;
+   uint16_t b;
+   uint16_t c;
+} triplet_t;
+
+typedef struct {
+   uint16_t count;
+   triplet_t triplets[];
+} triplets_t;
+
+triplets_t* triplets_with_sum(uint16_t n);
+void free_triplets(triplets_t *triplets);
 #endif
