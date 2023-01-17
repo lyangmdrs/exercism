9f56f02d3a6e5f940dbf169008a244a6bb080ca6
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-01-17 17:46:39 -0300

    :heavy_check_mark: all tests passing

--- c/pythagorean-triplet/pythagorean_triplet.c
+++ c/pythagorean-triplet/pythagorean_triplet.c
@@ -1,5 +1,7 @@
 #include "pythagorean_triplet.h"
 
+bool is_a_triplet(uint16_t a, uint16_t b, uint16_t c, uint16_t sum);
+
 triplets_t* triplets_with_sum(uint16_t n)
 {
     triplets_t* triplet = malloc(sizeof(triplets_t) + sizeof(triplet_t));
@@ -9,33 +11,32 @@ triplets_t* triplets_with_sum(uint16_t n)
         return NULL;
     }
 
+    uint16_t start_counting = 3;
+    uint16_t end_counting = n/2;
+
     triplet->count = 0;
 
-    for (uint16_t i = 2; i < n; i++)
+    for (uint16_t a = start_counting; a < end_counting; a++)
     {
-        for (uint16_t j = i + 1; j < n; j++)
+        for (uint16_t b = a + 1; b < end_counting; b++)
         {
-            for (uint16_t k = j + 1; k < n; k++)
+            uint16_t c = sqrt(pow(a, 2) + pow(b, 2));
+
+            if (is_a_triplet(a, b, c, n))
             {
-                if ((pow(i, 2) + pow(j, 2)) == pow(k, 2))
+                triplet->count++;
+                triplets_t* _triplet = realloc(triplet, sizeof(triplets_t) + sizeof(triplet_t) * triplet->count);
+
+                if (!_triplet)
                 {
-                    if ((i + j + k) == n)
-                    {
-                        triplet->count++;
-                        triplets_t* _triplet = realloc(triplet, sizeof(triplets_t) + sizeof(triplet_t) * triplet->count);
-
-                        if (!triplet)
-                        {
-                            return NULL;
-                        }
-
-                        triplet = _triplet;
-
-                        triplet->triplets[triplet->count - 1].a = i;
-                        triplet->triplets[triplet->count - 1].b = j;
-                        triplet->triplets[triplet->count - 1].c = k;
-                    }
+                    return NULL;
                 }
+
+                triplet = _triplet;
+
+                triplet->triplets[triplet->count - 1].a = a;
+                triplet->triplets[triplet->count - 1].b = b;
+                triplet->triplets[triplet->count - 1].c = c;
             }
         }
     }
@@ -46,4 +47,12 @@ triplets_t* triplets_with_sum(uint16_t n)
 void free_triplets(triplets_t *triplets)
 {
     free(triplets);
+}
+
+bool is_a_triplet(uint16_t a, uint16_t b, uint16_t c, uint16_t sum)
+{
+    bool right_sequence = (a < b) && (b < c);
+    bool right_sum = (a + b + c) == sum;
+    bool pytagorean_relation = (pow(a, 2) + pow(b, 2)) == pow(c, 2);
+    return right_sequence && right_sum && pytagorean_relation;
 }
