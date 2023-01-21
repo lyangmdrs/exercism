5f1fd9220a3e962f572929be04fd649cb15fc549
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-01-21 00:26:20 -0300

    :construction: 1st test passing

--- c/allergies/allergies.c
+++ c/allergies/allergies.c
@@ -1,1 +1,10 @@
 #include "allergies.h"
+
+bool is_allergic_to(allergen_t allergn, uint32_t score)
+{
+    if (allergn == 1 && score == 0)
+    {
+        return false;
+    }
+    return true;
+}
--- c/allergies/allergies.h
+++ c/allergies/allergies.h
@@ -2,17 +2,18 @@
 #define ALLERGIES_H
 
 #include <stdbool.h>
+#include <stdint.h>
 
 typedef enum {
-   ALLERGEN_EGGS = 0,
-   ALLERGEN_PEANUTS,
-   ALLERGEN_SHELLFISH,
-   ALLERGEN_STRAWBERRIES,
-   ALLERGEN_TOMATOES,
-   ALLERGEN_CHOCOLATE,
-   ALLERGEN_POLLEN,
-   ALLERGEN_CATS,
-   ALLERGEN_COUNT,
+   ALLERGEN_EGGS = 1,
+   ALLERGEN_PEANUTS = 2,
+   ALLERGEN_SHELLFISH = 4,
+   ALLERGEN_STRAWBERRIES = 8,
+   ALLERGEN_TOMATOES = 16,
+   ALLERGEN_CHOCOLATE = 32,
+   ALLERGEN_POLLEN = 64,
+   ALLERGEN_CATS = 128,
+   ALLERGEN_COUNT = 256,
 } allergen_t;
 
 typedef struct {
@@ -20,4 +21,6 @@ typedef struct {
    bool allergens[ALLERGEN_COUNT];
 } allergen_list_t;
 
+bool is_allergic_to(allergen_t allergn, uint32_t score);
+allergen_list_t get_allergens(uint32_t score);
 #endif
