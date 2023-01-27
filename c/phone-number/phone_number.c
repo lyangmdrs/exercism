6e62899cb56c33b7129bad943bdcfd7c8fa7e221
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-01-26 21:34:38 -0300

    :construction: 7th test passing

--- c/phone-number/phone_number.c
+++ c/phone-number/phone_number.c
@@ -4,6 +4,20 @@ void prepend(char* string,  char sufix);
 
 char *phone_number_clean(const char *input)
 {
+    if (strlen(input) <= PHONE_STRING_LEN)
+    {
+        char* invalid_input_result = malloc(sizeof(char) * PHONE_STRING_LEN);
+        sprintf(invalid_input_result, "%s", "0000000000");
+        return invalid_input_result;
+    }
+
+    if ((strlen(input) == PHONE_STRING_LEN + 1) && input[0] != '1')
+    {
+        char* invalid_input_result = malloc(sizeof(char) * PHONE_STRING_LEN);
+        sprintf(invalid_input_result, "%s", "0000000000");
+        return invalid_input_result;
+    }
+    
     char* result = malloc(sizeof(char));
 
     sprintf(result, "%c", '\0');
--- c/phone-number/test_phone_number.c
+++ c/phone-number/test_phone_number.c
@@ -46,7 +46,6 @@ static void test_cleans_numbers_with_multiple_spaces(void)
 
 static void test_invalid_when_9_digits(void)
 {
-   TEST_IGNORE();
    const char input[] = "123456789";
    const char expected[] = "0000000000";
 
@@ -57,7 +56,6 @@ static void test_invalid_when_9_digits(void)
 
 static void test_invalid_when_11_digits_does_not_start_with_a_1(void)
 {
-   TEST_IGNORE();
    const char input[] = "22234567890";
    const char expected[] = "0000000000";
 
@@ -68,7 +66,6 @@ static void test_invalid_when_11_digits_does_not_start_with_a_1(void)
 
 static void test_valid_when_11_digits_and_starting_with_1(void)
 {
-   TEST_IGNORE();
    const char input[] = "12234567890";
    const char expected[] = "2234567890";
 
@@ -80,7 +77,6 @@ static void test_valid_when_11_digits_and_starting_with_1(void)
 static void
 test_valid_when_11_digits_and_starting_with_1_even_with_punctuation(void)
 {
-   TEST_IGNORE();
    const char input[] = "+1 (223) 456-7890";
    const char expected[] = "2234567890";
 
