a2e2238b7dc369d33e5e7cd00342cbebdcdb0b05
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-01-31 17:03:19 -0300

    :ok_hand: style: removes debug commentaries

--- c/clock/clock.c
+++ c/clock/clock.c
@@ -27,25 +27,15 @@ clock_t clock_create(int hour, int minute)
 
     sprintf(new_clock.text, "%s", RESETED_CLK);
 
-    // printf("hours: %d - minutes %d\n", hour, minute);
-    // printf("CLOCK: %s\n", new_clock.text);
-    // printf("H_DOZENS: %d\n", hour_dozens);
-    // printf("M_DOZENS: %d\n", min_dozens);
-
     new_clock.text[0] += hour_dozens;
-    // printf("CLOCK: %s\n", new_clock.text);
     new_clock.text[1] += (hour - hour_dozens * 10);
-    // printf("CLOCK: %s\n", new_clock.text);
     new_clock.text[3] += min_dozens;
-    // printf("CLOCK: %s\n", new_clock.text);
     new_clock.text[4] += (minute - min_dozens * 10);
-    // printf("CLOCK: %s\n", new_clock.text);
 
     return new_clock;
 }
 
-/** from this line functions were written without any functionality
- *  just to declare the functions and let the compiler happy. **/
+
 clock_t clock_add(clock_t clock, int minute_add)
 {
     if (minute_add)
@@ -55,6 +45,8 @@ clock_t clock_add(clock_t clock, int minute_add)
     return clock;
 }
 
+/** from this line functions were written without any functionality
+ *  just to declare the functions and let the compiler happy. **/
 clock_t clock_subtract(clock_t clock, int minute_subtract)
 {
     if (minute_subtract)
