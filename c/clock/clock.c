336a3de741626156fc2b4c637b79fe533cb75b3e
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-01-31 12:24:34 -0300

    :construction: chore: add declared functions with dummy code
    declare the prototyped functions and let the compiler happy

--- c/clock/clock.c
+++ c/clock/clock.c
@@ -14,11 +14,48 @@ clock_t clock_create(int hour, int minute)
         return new_clock;
     }
 
+    // printf("CLOCK: %s\n", new_clock.text);
+    // printf("H_DOZENS: %d\n", hour_dozens);
+    // printf("M_DOZENS: %d\n", min_dozens);
+
     new_clock.text[0] += hour_dozens;
+    // printf("CLOCK: %s\n", new_clock.text);
     new_clock.text[1] += (hour - hour_dozens * 10);
+    // printf("CLOCK: %s\n", new_clock.text);
     new_clock.text[3] += min_dozens;
+    // printf("CLOCK: %s\n", new_clock.text);
     new_clock.text[4] += (minute - min_dozens * 10);
+    // printf("CLOCK: %s\n", new_clock.text);
 
     return new_clock;
 }
 
+/** from this line functions were written without any functionality
+ *  just to declare the functions and let the compiler happy. **/
+clock_t clock_add(clock_t clock, int minute_add)
+{
+    if (minute_add)
+    {
+        return clock;
+    }
+    return clock;
+}
+
+clock_t clock_subtract(clock_t clock, int minute_subtract)
+{
+    if (minute_subtract)
+    {
+        return clock;
+    }
+    return clock;
+}
+
+bool clock_is_equal(clock_t a, clock_t b)
+{
+    if (strcmp(a.text, b.text))
+    {
+        return false;
+    }
+
+    return true;
+}
