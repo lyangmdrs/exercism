ef1bf48daf1b2c292da64815a6a68418cbce2cea
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-01-31 12:15:39 -0300

    :bug: fix: fix the creation of new clocks

--- c/clock/clock.c
+++ c/clock/clock.c
@@ -5,14 +5,18 @@ clock_t clock_create(int hour, int minute)
     clock_t new_clock;
 
     int hour_dozens = hour/10;
-    int min_dozens = hour/10;
+    int min_dozens = minute/10;
 
     sprintf(new_clock.text, "%s", RESETED_CLK);
 
     new_clock.text[0] += hour_dozens;
-    new_clock.text[1] += (hour - hour_dozens);
+    
+    new_clock.text[1] += (hour - hour_dozens * 10);
+    
     new_clock.text[3] += min_dozens;
-    new_clock.text[4] += (minute - min_dozens);
+    
+    new_clock.text[4] += (minute - min_dozens * 10);
+    
 
     return new_clock;
 }
