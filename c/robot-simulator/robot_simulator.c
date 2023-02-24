9e9863c9759eacc2e53e0f5a8e9520b8092fb472
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-02-24 00:18:38 -0300

    :white_check_mark: test: 1 test passing

--- c/robot-simulator/robot_simulator.c
+++ c/robot-simulator/robot_simulator.c
@@ -1,1 +1,10 @@
 #include "robot_simulator.h"
+
+robot_status_t robot_create(robot_direction_t direction, int x, int y)
+{
+    robot_status_t new_robot = {.direction = direction,
+                                .position.x = x,
+                                .position.y = y};
+    
+    return new_robot;
+}
