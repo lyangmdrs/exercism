395cb713e5fc9bc79b4c32022a5ce64511e568a3
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-02-27 17:56:36 -0300

    :white_check_mark: test: all tests passing

--- c/binary/binary.c
+++ c/binary/binary.c
@@ -1,1 +1,29 @@
 #include "binary.h"
+
+#include <string.h>
+#include <math.h>
+
+#include <stdio.h>
+
+int convert(const char *input)
+{
+    size_t input_len = strlen(input);
+    
+    int result = 0;
+
+    for (size_t i = 0; i < input_len; i++)
+    {
+        if ((input[i] > '1') || (input[i] < '0'))
+        {
+            return INVALID;
+        }
+
+        if(input[i] == '1')
+        {
+            result += pow(2, input_len - i - 1);
+        }
+    }
+
+    return result;
+    
+}
