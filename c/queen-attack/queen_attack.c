930ab616069394cc001e2ac0cb0171d80d176c1c
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-02-27 19:21:10 -0300

    :white_check_mark: 9 tests passing

--- c/queen-attack/queen_attack.c
+++ c/queen-attack/queen_attack.c
@@ -1,6 +1,7 @@
 #include "queen_attack.h"
 
 #include <stdbool.h>
+#include <stdio.h>
 
 bool position_is_valid(position_t queen);
 
@@ -21,6 +22,24 @@ attack_status_t can_attack(position_t queen_1, position_t queen_2)
         return CAN_ATTACK;
     }
 
+    // Look for queens on 1st diagonal
+    for (int i = queen_1.column, j = queen_1.row; (i < 8) && (j >= 0); i++, j--)
+    {
+        if ((queen_2.column == i) && (queen_2.row == j))
+        {
+            return CAN_ATTACK;
+        }
+    }
+
+    printf("decrescent!\n");
+    for (int i = queen_1.column, j = queen_1.row; (i >= 0) && (j < 8); i--, j++)
+    {   
+        if ((queen_2.column == i) && (queen_2.row == j))
+        {
+            return CAN_ATTACK;
+        }
+    }
+
     
 
     return CAN_NOT_ATTACK;
@@ -28,7 +47,7 @@ attack_status_t can_attack(position_t queen_1, position_t queen_2)
 
 bool position_is_valid(position_t queen)
 {
-    if ((queen.column == 0) || (queen.row == 0))
+    if ((queen.column >= 8) || (queen.row >= 8))
     {
         return false;
     }
--- c/queen-attack/test_queen_attack.c
+++ c/queen-attack/test_queen_attack.c
@@ -115,7 +115,6 @@ static void test_can_attack_on_same_column(void)
 
 static void test_can_attack_on_first_diagonal(void)
 {
-   TEST_IGNORE();
    position_t white_queen;
    position_t black_queen;
 
