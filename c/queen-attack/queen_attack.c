47000751a310091fa3e688114daca8616843b23a
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-02-27 18:32:37 -0300

    :white_check_mark: 2 tests passing

--- c/queen-attack/queen_attack.c
+++ c/queen-attack/queen_attack.c
@@ -1,12 +1,31 @@
 #include "queen_attack.h"
 
+#include <stdbool.h>
+
+bool position_is_valid(position_t queen);
+
 attack_status_t can_attack(position_t queen_1, position_t queen_2)
 {
-    // Check if positions are equal
     if ((queen_1.column == queen_2.column) && (queen_1.row == queen_2.row))
     {
         return INVALID_POSITION;
     }
 
-    return INVALID_POSITION;
+    if (!position_is_valid(queen_1) || !position_is_valid(queen_2))
+    {
+        return INVALID_POSITION;
+    }
+    
+
+    return CAN_ATTACK;
+}
+
+bool position_is_valid(position_t queen)
+{
+    if ((queen.column == 0) || (queen.row == 0))
+    {
+        return false;
+    }
+
+    return true;
 }
