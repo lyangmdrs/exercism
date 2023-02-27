9bf1b98bd1303a1aa16795aec100d136625f2f2a
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-02-27 18:16:47 -0300

    :white_check_mark: 1 test passing

--- c/queen-attack/queen_attack.c
+++ c/queen-attack/queen_attack.c
@@ -1,1 +1,12 @@
 #include "queen_attack.h"
+
+attack_status_t can_attack(position_t queen_1, position_t queen_2)
+{
+    // Check if positions are equal
+    if ((queen_1.column == queen_2.column) && (queen_1.row == queen_2.row))
+    {
+        return INVALID_POSITION;
+    }
+
+    return INVALID_POSITION;
+}
