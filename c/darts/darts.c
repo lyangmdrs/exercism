f882ffc35be4b430123a8987460d967dc43f5d4f
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-02-28 17:28:38 -0300

    :white_check_mark: test: 2 tests passing

--- c/darts/darts.c
+++ c/darts/darts.c
@@ -1,7 +1,31 @@
 #include "darts.h"
 
-uint8_t score(coordinate_t position)
+/**
+ * The outer circle has a radius of 10 units, this circle gives 1 point;
+ * The middle circle has a radius of 5 units, this circle gives 5 points;
+ * The inner circle has a radius of 1 unit, this circle gives 10 points;
+ * 
+ * All the circles are concentric at the coordinates (0,0);
+ */
+
+#define OUTER_CIRCLE_RADIUS (10.0F)
+#define MIDDLE_CIRCLE_RADIUS (5.0F)
+#define INNER_CIRCLE_RADIUS (1.0F)
+
+enum {MISS, OUTER_CIRCLE, MIDDLE_CIRCLE, INNER_CIRCLE};
+
+uint8_t score(coordinate_t coordinate)
 {
-    if (position.x) {}
-    return 0;
+    if (fabs(coordinate.x) > 8 && fabs(coordinate.y) >= 8)
+    {
+        return MISS;
+    }
+
+    if ((fabs(coordinate.x) > MIDDLE_CIRCLE_RADIUS) ^
+        (fabs(coordinate.y) > MIDDLE_CIRCLE_RADIUS))
+    {
+        return OUTER_CIRCLE;
+    }
+
+    return MISS;
 }
--- c/darts/darts.h
+++ c/darts/darts.h
@@ -2,12 +2,14 @@
 #define DARTS_H
 
 #include <stdint.h>
+#include <math.h>
+
 typedef struct 
 {
     float x;
     float y;
 } coordinate_t;
 
-uint8_t score(coordinate_t position);
+uint8_t score(coordinate_t coordinate);
 
 #endif
