86090a95ad9a4de2ad359403d13c2bc8d12b52d9
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-03-03 17:03:04 -0300

    :bug: fix: returns a null list if malloc fails

--- c/linked-list/linked_list.c
+++ c/linked-list/linked_list.c
@@ -2,6 +2,10 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+#define INSERT_NODE_FAIL 0
+#define INSERT_NODE_SUCCESS 1
 
 typedef struct list_node
 {
@@ -27,9 +31,11 @@ static ll_data_t unlink_node(list_node_t *node, list_t *list)
    return data;
 }
 
-static void insert_node(list_node_t *preceding, list_t *list, ll_data_t item_data)
+static bool insert_node(list_node_t *preceding, list_t *list, ll_data_t item_data)
 {
    list_node_t *new_node = malloc(sizeof(list_node_t));
+
+   if (!new_node) return INSERT_NODE_FAIL;
    
    new_node->prev = preceding->prev;
    preceding->prev = new_node;
@@ -38,13 +44,20 @@ static void insert_node(list_node_t *preceding, list_t *list, ll_data_t item_dat
    new_node->data = item_data;
 
    list->count++;
+
+   return INSERT_NODE_SUCCESS;
 }
 
 list_t *list_create(void)
 {
    list_t *new_list = malloc(sizeof(list_t));
+   if (!new_list) return NULL;
+
    new_list->first = malloc(sizeof(list_node_t));
+   if (!new_list->first) return NULL;
+   
    new_list->last = malloc(sizeof(list_node_t));
+   if (!new_list->last) return NULL;
 
    new_list->first->prev = NULL;
    new_list->first->next = new_list->last;   
@@ -62,7 +75,13 @@ size_t list_count(const list_t *list)
 
 void list_push(list_t *list, ll_data_t item_data)
 {
-   insert_node(list->last, list, item_data);
+   bool success = insert_node(list->last, list, item_data);
+
+   if (!success)
+   {
+      list_destroy(list);
+      list = NULL;
+   }
 }
 
 ll_data_t list_pop(list_t *list)
@@ -74,7 +93,13 @@ ll_data_t list_pop(list_t *list)
 
 void list_unshift(list_t *list, ll_data_t item_data)
 {
-   insert_node(list->first->next, list, item_data);
+   bool success = insert_node(list->first->next, list, item_data);
+
+   if (!success)
+   {
+      list_destroy(list);
+      list = NULL;
+   }
 }
 
 ll_data_t list_shift(list_t *list)
