0f3656a137e98428a946c185f1d82c1c2784d91e
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-03-07 19:18:05 -0300

    :white_check_mark: test: 2nd test passing

--- c/pangram/pangram.c
+++ c/pangram/pangram.c
@@ -1,8 +1,14 @@
 #include "pangram.h"
 
+#include <string.h>
 bool is_pangram(const char *sentence)
 {
     if (!sentence)
+    {
+        return false;
+    }
+
+    if (strcmp(sentence, "") == 0)
     {
         return false;
     }
