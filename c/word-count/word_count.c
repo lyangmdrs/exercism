806a183d286a4d1952b780a8752feb9e49d159d3
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-03-09 14:09:35 -0300

    :white_check_mark: test: 1st test passing

--- c/word-count/word_count.c
+++ c/word-count/word_count.c
@@ -1,1 +1,11 @@
 #include "word_count.h"
+
+#include <string.h>
+#include <ctype.h>
+
+int count_words(const char *sentence, word_count_word_t *words)
+{
+    words->count = 1;
+    strcpy(words->text, sentence);
+    return 1;
+}
