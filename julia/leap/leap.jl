dac3a10105cce5b9687eb6ca79927f21950a0693
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-04-19 13:09:42 -0300

    :white_check_mark: test: all tests passing

--- julia/leap/leap.jl
+++ julia/leap/leap.jl
@@ -5,6 +5,9 @@ Return `true` if `year` is a leap year in the gregorian calendar.
 
 """
 function is_leap_year(year)
-
+    if (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+        return true
+    end
+    return false
 end
 
