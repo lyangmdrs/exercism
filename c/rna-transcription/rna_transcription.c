4052cf427560b4da7ace660c2322a0758cd09213
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-04-20 08:36:13 -0300

    :white_check_mark: test: 1 test passing

--- c/rna-transcription/rna_transcription.c
+++ c/rna-transcription/rna_transcription.c
@@ -1,1 +1,46 @@
 #include "rna_transcription.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_STR_LEN 256
+#define INITIAL_RESULT "A:0 C:0 G:0 T:0"
+#define RESULT_STR "A:%ld C:%ld G:%ld T:%ld"
+
+char *to_rna(const char *dna)
+{
+    char *result = malloc(MAX_STR_LEN * sizeof(char));
+
+    if (!result)
+    {
+        return NULL;
+    }
+
+    result[0] = '\0';
+
+    if (dna[0] == '\0')
+    {
+        return result;
+    }
+
+    for (size_t i = 0; i < strlen(dna); i++)
+    {
+        switch (dna[i])
+        {
+        case 'A':
+            break;
+        case 'C':
+            strcat(result, "G");
+            break;
+        case 'G':
+            break;
+        case 'T':
+            break;
+        default:
+            result[0] = '\0';
+            return result;
+        }
+    }
+
+    return result;
+}
