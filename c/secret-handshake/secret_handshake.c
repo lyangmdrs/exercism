3963754c55198b508d745f2f42790c2e128c34f0
Lyang Medeiros <lyang.mdrs@gmail.com>
2023-04-21 23:36:53 -0300

    :white_check_mark: test: 1 test passing

--- c/secret-handshake/secret_handshake.c
+++ c/secret-handshake/secret_handshake.c
@@ -1,1 +1,21 @@
 #include "secret_handshake.h"
+#include <stdlib.h>
+#include <string.h>
+
+#define NUM_SIGNALS 4
+
+static const char *signals[] = {"wink", "double blink", "close your eyes", "jump"};
+
+const char **commands(size_t number)
+{
+    const char **result = malloc(NUM_SIGNALS * sizeof(char*));
+
+    if (!result)
+    {
+        return NULL;
+    }
+    if(number){}
+    result[0] = signals[0];
+    
+    return result;
+}
